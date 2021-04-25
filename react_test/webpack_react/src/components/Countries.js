4bd93480cef05071a23325093c23131847e5faa4
dendzen <fedosov555@gmail.com>
2021-04-25 11:37:09 +0300

    1

--- react_test/webpack_react/src/components/Countries.js
+++ react_test/webpack_react/src/components/Countries.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/app.css";
 import axios from "axios";
 
@@ -7,6 +7,7 @@ function Countries () {
     if (!countries.length) {
         axios.get("https://restcountries.eu/rest/v2/all").then(res => {
             console.log(res);
+            setCountries(res.data);
         });
 }
 
@@ -14,7 +15,11 @@ function Countries () {
             <table>
                 <thead><tr><th>Name</th><th>Capital</th></tr></thead>
                 <tbody>
-                {countries.map(country => <tr><td>{country.name}</td><td>{country.capital}</td></tr>)}
+                {countries.map(country =>
+                    <tr>
+                        <td>{country.name}</td>
+                        <td>{country.capital}</td>
+                    </tr>)}
                 </tbody>
             </table>
     );
