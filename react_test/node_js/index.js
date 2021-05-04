95d0549e19f64e674227434d1fb00cf7ebd976cd
dendzen <fedosov555@gmail.com>
2021-05-04 16:25:13 +0300

    1

--- react_test/node_js/index.js
+++ react_test/node_js/index.js
@@ -3,12 +3,13 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const argv = yargs
- .command(['cretae <filename> [content]', 'c'], 'Cretae a new file and write passed content to it', {}, (argv) => {
-    const filepath = path.resolve(argv.filename);
-    const defaultContent = 'Default Content';
-    fs.writeFile(filepath, argv.content || defaultContent)
-        .then(() => console.log('Your data saved'))
-        .catch(e => console.error('An Error', e));
+    .command(['cretae <filename> [content]', 'c'], 'Cretae a new file and write passed content to it', {}, (argv) => {
+        const filepath = path.resolve(argv.filename);
+        const defaultContent = 'Default Content';
+        fs.writeFile(filepath, argv.content || defaultContent)
+            .then(() => console.log('Your data saved'))
+            .catch(e => console.error('An Error', e));
     })
-.argv;
+    .argv;
+
 
